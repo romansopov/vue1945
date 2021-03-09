d78d4b62fdf2031a4eb195af3bb7faea4e1706f9
FelixIzm <for_cv@mail.ru>
2021-03-09 22:15:25 +0300

    modify

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import RequestForm from '../views/Obd-Memorial.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +12,12 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/obd-memorial',
+    name: 'Obd-memorial',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: RequestForm
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="sendData">
+      <div class="form-control">
+        <label for="unit">Воинское подразделение</label>
+        <input v-model="unit" id="unit" type="text" />
+      </div>
+      <div class="form-control">
+        <label for="period">Период выборки</label>
+        <date-picker
+          v-model="period"
+          ref="datePicker"
+          name="contract_period"
+          range
+          format="DD-MM-YYYY"
+          valueType="format"
+          confirm
+          width="100%"
+          :placeholder="period"
+        ></date-picker>
+      </div>
+
+      <input type="submit" class="send" value="Send" />
+        <div id="info">
+          {{ info }}
+        </div>
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DatePicker from "vue2-datepicker";
+import "vue2-datepicker/index.css";
+import "vue2-datepicker/locale/ru";
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: { DatePicker },
+  data() {
+    return {
+      //period: [new Date(String('08-01-2019')), new Date(String('08-30-2019'))],
+      //period: [new Date(2019, 7, 1), new Date(2019, 7, 30)],
+      period: [],
+      unit: "2345",
+    };
+  },
+  methods: {
+    sendData() {
+      console.log(this.unit, this.period);
+      axios
+        .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        .then((response) => (this.info = response));
+    },
+  },
+
+  mounted() {
+    this.$refs.datePicker.currentValue = [
+      new Date(String("01-01-1939")),
+      new Date(String("12-31-1946")),
+    ];
+    //this.$refs.datePicker.placeholder = [new Date(String('01-01-1939')), new Date(String('12-31-1946'))];
+    //this.value = [new Date(String('08-01-2019')), new Date(String('08-30-2019'))];
+  },
+};
 </script>
+
+<style>
+.form-control {
+  padding: 5px;
+}
+.form-control label {
+  display: block;
+}
+.send {
+  margin: 5px;
+}
+</style>
