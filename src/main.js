7cd9c0bb879c813fd5d2e6de272d0e2497d281d7
roman.sopov <roman.sopov@bpmobile.com>
2021-03-18 23:48:33 +0300

    Fix table

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import router from './router'
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,83 +3,85 @@
     <form @submit.prevent="sendData">
       <div class="form-control">
         <label for="unit">Воинское подразделение</label>
-        <input v-model="unit" id="unit" type="text" />
+        <input v-model="unit" id="unit" type="text"/>
       </div>
       <div class="form-control">
         <label for="period">Период выборки</label>
         <date-picker
-          v-model="period"
-          ref="datePicker"
-          name="contract_period"
-          range
-          format="DD-MM-YYYY"
-          valueType="format"
-          confirm
-          width="100%"
+            v-model="period"
+            ref="datePicker"
+            name="contract_period"
+            range
+            format="DD-MM-YYYY"
+            valueType="format"
+            confirm
+            width="100%"
         ></date-picker>
       </div>
 
-      <input type="submit" class="send" value="Send" />
+      <input type="submit" class="send" value="Send"/>
       <div id="info">
         {{ info }}
       </div>
       <div id="tableunit">
-        <tableunit></tableunit>
+        <TableUnit></TableUnit>
       </div>
     </form>
   </div>
 </template>
 
 <script>
-import DatePicker from "vue2-datepicker";
-import "vue2-datepicker/index.css";
-import "vue2-datepicker/locale/ru";
+import DatePicker from 'vue2-datepicker'
+import 'vue2-datepicker/index.css'
+import 'vue2-datepicker/locale/ru'
 //import axios from "axios";
 //import Vuetify from 'vuetify';
 
 
-import tableunit from "@/components/TableUnit.vue";
+import TableUnit from '@/components/TableUnit.vue'
 
 export default {
-  components: { DatePicker,tableunit },
+  components: { DatePicker, TableUnit },
   data() {
     return {
       //period: [new Date(String('08-01-2019')), new Date(String('08-30-2019'))],
       //period: [new Date(2019, 7, 1), new Date(2019, 7, 30)],
       period: [],
-      unit: "2345",
-      info: "",
+      unit: '2345',
+      info: '',
       tableunit: this.tableunit
-    };
+    }
   },
   methods: {
     sendData() {
-      console.log(this.unit, this.period, this.tableunit);
-      this.tableunit = "time";
+      console.log(this.unit, this.period, this.tableunit)
+      this.tableunit = 'time'
       //this.TableUnit= TableUnit;
-/*       axios
-        .get("http://localhost:8000/unit/:rdf/:dd")
-        .then((response) => (this.info = "response"));
- */        
+      /*       axios
+              .get("http://localhost:8000/unit/:rdf/:dd")
+              .then((response) => (this.info = "response"));
+       */
     },
   },
 
   mounted() {
     this.$refs.datePicker.currentValue = [
-      new Date(String("01-01-1939")),
-      new Date(String("12-31-1946")),
-    ];
+      new Date(String('01-01-1939')),
+      new Date(String('12-31-1946')),
+    ]
   },
-};
+}
 </script>
 
 <style>
 .form-control {
   padding: 5px;
 }
+
 .form-control label {
   display: block;
 }
+
 .send {
   margin: 5px;
 }
